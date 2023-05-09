0a8f60e9a9e28de94c2567187bcf29f56fdd1770
RuslanIvasiunko <115837907+Rustavas@users.noreply.github.com>
2023-05-09 15:41:41 +0300

    summury

---
+++ js/module-2.js
@@ -0,0 +1,111 @@
+// 1======================
+// 1======================
+// 1======================
+// function checkAge(age) {
+//   if (age >= 18) { // Change this line
+//     return "You are an adult";
+//   }
+
+//   return "You are a minor";
+// }
+// 2======================
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+//   // Change code below this line
+
+//   if (password === ADMIN_PASSWORD) {
+//     return "Welcome!";
+//   } 
+//   return "Access denied, wrong password!";
+//   // Change code above this line
+// }
+// 3======================
+// function checkStorage(available, ordered) {
+//   // Change code below this line
+//   if (ordered === 0) {
+//     return "Your order is empty!";
+//   } if (ordered > available) {
+//     return "Your order is too large, not enough goods in stock!";
+//   }
+//     return "The order is accepted, our manager will contact you";
+//   // Change code above this line
+// }
+// 4======================
+// 5======================
+// 6======================
+// 7======================
+// 8======================
+// 9======================
+// function getExtremeElements(array) {
+  // Change code below this line
+
+// return [array[0], array[array.length-1]]
+  // Change code above this line
+// }
+// 10======================
+// function splitMessage(message, delimiter) {
+  // let words;
+  // Change code below this line
+// words = message.split(delimiter);
+  // Change code above this line
+  // return words;
+// }
+// 11======================
+// function calculateEngravingPrice(message, pricePerWord) {
+  // Change code below this line
+// const allPrice = message.split(" ").length * pricePerWord;
+//  return allPrice;
+  // Change code above this line
+// }
+// 12======================
+// function makeStringFromArray(array, delimiter) {
+  // let string;
+  // Change code below this line
+// string = array.join(delimiter);
+  // Change code above this line
+  // return string;
+// }
+// 13======================
+// function slugify(title) {
+  // Change code below this line
+// const slug = title.toLowerCase().split(" ").join("-");
+// console.log(slug);
+  // Change code above this line
+// }
+// slugify('Arrays for begginers')
+// 14======================
+// const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+// Change code below this line
+// const firstTwoEls = fruits.slice(0, 2);
+// const nonExtremeEls = fruits.slice(1, fruits.length - 1);
+// const lastThreeEls = fruits.slice(fruits.length-3, fruits.length);
+// console.log(firstTwoEls)
+// console.log(nonExtremeEls)
+// console.log(lastThreeEls)
+// 15======================
+// 16======================
+// 17======================
+// 18======================
+// function calculateTotal(number) {
+  
+//    let sum = 0;
+//  for(let i = 1; i <= number; i += 1){
+   
+//   sum += i;
+//  }
+//  console.log(sum); 
+//  }
+//  calculateTotal(122)
+// 19======================
+const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+for (let i = 0; i <= fruits.length-1; i = 1) { 
+  const fruit = fruit[i];
+  console.log(fruit);
+}
+// 1======================
+// 1======================
+// 1======================
+// 1======================
+// 1======================
+// 1======================
